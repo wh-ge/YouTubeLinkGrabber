f315d6936ab895692fae2c8f6431b8323cee3ae7
wh-ge <128788764+wh-ge@users.noreply.github.com>
2025-02-06 15:57:32 +0100

    Update YouTubeLinkGrabber.py

--- YouTubeLinkGrabber.py
+++ YouTubeLinkGrabber.py
@@ -7,7 +7,7 @@ from bs4 import BeautifulSoup
 
 def grab_youtube(url: str):
     """
-        Grabs the live-streaming M3U8 file from YouTube
+Grabs the live-streaming M3U8 file from YouTube
         :param url: The YouTube URL of the livestream
     """
     print(f'\n#url: {url}')
