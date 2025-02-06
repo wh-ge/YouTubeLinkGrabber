3f74f0117f6e46531954d73862a315d1fa9bb898
wh-ge <128788764+wh-ge@users.noreply.github.com>
2025-02-06 16:47:34 +0100

    Update YouTubeLinkGrabber.py

--- YouTubeLinkGrabber.py
+++ YouTubeLinkGrabber.py
@@ -44,7 +44,7 @@ channel_id = ''
 category = ''
 
 # Open text file and parse stream information and URL
-with open('./youtubeList.txt', encoding='utf-8') as f:
+with open('./youtubeLink.txt', encoding='utf-8') as f:
     print("#EXTM3U")
     for line in f:
         line = line.strip()
